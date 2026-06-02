a789703fc46f94b9054ec152095b9c53027605ea
koltsis insurance advisor <koltsis2002@gmail.com>
2026-06-03 00:03:18 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,8 +9,8 @@
  * Όταν αλλάζεις κώδικα, ΑΛΛΑΞΕ το CACHE_VERSION για να φορτώσουν τα νέα αρχεία.
  */
 
-const CACHE_VERSION = 'nn-questionnaire-v8';
-const RUNTIME_CACHE = 'nn-runtime-v8';
+const CACHE_VERSION = 'nn-questionnaire-v9';
+const RUNTIME_CACHE = 'nn-runtime-v9';
 
 // Αρχεία που φορτώνουν αμέσως όταν εγκατασταθεί η εφαρμογή
 const APP_SHELL = [
